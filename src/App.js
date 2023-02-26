b3d65f8261c5fd8c339dab80fa52839cfa65e82b
ingucell <mathiaslawson70@gmail.com>
2023-02-26 22:02:16 +0000

    material theme setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { ColorModeContext, useMode } from "./theme";
+import {Routes, Route} from 'react-router-dom'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
+  const [theme, colorMode] = useMode()
+
   return (
    <>
+     <ColorModeContext.Provider value = {colorMode}>
+          <ThemeProvider theme={theme}>
+               <CssBaseline />
+                   <div className="app">
+            
+            <main className="content">
+                  Mathias
+
+            </main>
+
+                   </div>
+          </ThemeProvider>
+     </ColorModeContext.Provider>
    </>
   );
 }
